f6e2df71f0518e78d18c63dc957eee21c7f7ef6e
Darren Xu <darrenxu1994@yahoo.com.au>
2020-09-20 18:46:24 +1000

    card changes

--- src/pages/landing/BlogCard/BlogCard.tsx
+++ src/pages/landing/BlogCard/BlogCard.tsx
@@ -1,26 +1,41 @@
 import React from 'react'
 import "./BlogCard.scss";
 import { Blog } from '../../../services/types';
+import { Link } from 'react-router-dom';
 interface IProps {
     data: Blog,
     children: any
 }
 export default function BlogCard({ data, ...children }: IProps) {
     return (
-        <div className="center">
-            <div className="property-card">
-                <a href="#">
-                    <div className="property-image" style={{ backgroundImage: `url(${data.MediaURL})` }}>
-                        <div className="property-image-title">
-                        </div>
-                    </div>
-                </a>
-                <div className="property-description">
+        // <div className="center" style={{ padding: "30px" }}>
+        //     <Link to={`/blog/${data._id}`}>
+
+        //         <div className="property-card">
+        //             <a href="#">
+        //                 <div className="property-image" style={{ backgroundImage: `url(${data.MediaURL})` }}>
+        //                     <div className="property-image-title">
+        //                     </div>
+        //                 </div>
+        //             </a>
+        //             <div className="property-description">
+        //                 <h5> {data.Title} </h5>
+        //                 <p>{data.Content}</p>
+        //             </div>
+
+        //         </div>
+        //     </Link>
+        // </div>
+        <div className="padding-outer">
+            <div className="padding-outer__div" style={{ display: "flex", backgroundColor: "white", width: "100%" }}>
+                <div style={{ backgroundImage: `url(${data.MediaURL})` }} className="side-img">
+                </div>
+                <div style={{ flex: 4, paddingLeft: "10px", paddingTop: "10px" }} className="side-div">
                     <h5> {data.Title} </h5>
                     <p>{data.Content}</p>
                 </div>
-
             </div>
+
         </div>
     )
 }
--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -3,6 +3,8 @@ import { Blog } from '../../services/types'
 import { StrapiAPI } from '../../services/StrapiAPI';
 import BlogCard from "./BlogCard/BlogCard";
 
+import "./Landing.scss";
+
 export default function Landing() {
     const [data, setdata] = useState<Blog[] | null>(null)
     useEffect(() => {
@@ -17,10 +19,16 @@ export default function Landing() {
     }
 
     return (
-        <div style={{ display: "flex", justifyContent: "space-evenly", flexWrap: "wrap" }}>
-            {data?.map(e => {
-                return <BlogCard key={e._id} data={e}>{e.Title}</BlogCard>
-            })}
+        <div >
+            <div className="heading" style={{ padding: "30px", fontSize: "40px", textAlign: "center" }}>
+                Choccy ratings with Daz
+            </div>
+            <div className="card-container" style={{ width: "80%", margin: "auto" }}>
+
+                {data?.map(e => {
+                    return <BlogCard key={e._id} data={e}>{e.Title}</BlogCard>
+                })}
+            </div>
         </div>
     )
 }
